dc0c6bb876c434cf884d94615b254b42e4879c22
samuel <samuel@localhost>
2020-03-06 12:48:08 +0100

    commento secondo commit

--- src/main/java/it/polito/tdp/prova1_2020/Main.java
+++ src/main/java/it/polito/tdp/prova1_2020/Main.java
@@ -1,5 +1,7 @@
 package it.polito.tdp.prova1_2020;
 
+//commento di prova per fare il secondo commit
+
 public class Main {
 	public static void main(String[] args) {
         EntryPoint.main(args);
